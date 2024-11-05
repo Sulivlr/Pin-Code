24e588acfc93b885710842173e19f3708dfb39ca
Sultan Mukhtarov <suli419956@gmail.com>
2024-11-06 01:32:45 +0600

    created user interface

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
+import {store} from './app/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <App/>
+  <Provider store={store}>
+    <App/>
+  </Provider>
 );
