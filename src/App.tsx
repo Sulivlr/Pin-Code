4c52c4a6856452795dc2bd66da7be83da319e468
Sultan Mukhtarov <suli419956@gmail.com>
2024-11-06 19:17:27 +0600

    created a slice of password. In 'writeDigit' if lenght of value lesser than 4, then action payload + value. deleteDigit = value will be = method slice = 0 initial value, -1 means deleting last value from the right. True password. state.isCorrect initial state is boolean or null, but if value === 1337. its true. And also i gave a type to action in generic,  so it will be number to make sure, nothing else will be in value.

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,10 @@
+import Password from './container/password.tsx';
+
 const App = () => {
   return (
     <>
       <main className="container-fluid">
-        <div className="container-fluid">
-          <div className="row justify-content-center">
-            <div className="col-md-6 text-center">
-              <h4>Enter Password here:</h4>
-            </div>
-          </div>
-          <div className="row justify-content-center mt-3">
-            <div className="col-md-6 text-center">
-              <h4></h4>
-            </div>
-          </div>
-          <div className="d-flex justify-content-center mt-3">
-            <div className="d-flex flex-column text-center">
-              <div className="btn-group-lg">
-                {[1, 2, 3].map((digit) => (
-                  <button key={digit} className="btn btn-primary m-1">
-                    {digit}
-                  </button>
-                ))}
-              </div>
-              <div className="btn-group-lg">
-                {[4, 5, 6].map((digit) => (
-                  <button key={digit} className="btn btn-primary m-1">
-                    {digit}
-                  </button>
-                ))}
-              </div>
-              <div className="btn-group-lg">
-                {[7, 8, 9].map((digit) => (
-                  <button key={digit} className="btn btn-primary m-1">
-                    {digit}
-                  </button>
-                ))}
-              </div>
-              <div>
-                <div className="btn-group-lg">
-                  {[0].map((digit) => (
-                    <button key={digit} className="btn btn-primary m-2">{digit}</button>
-                  ))}
-                </div>
-                <button className="btn btn-warning me-3">
-                  Delete
-                </button>
-                <button className="btn btn-success">
-                  Enter
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Password />
       </main>
     </>
   );
---
+++ src/container/passwordSlice.ts
@@ -0,0 +1,37 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+interface PasswordState {
+  value: string;
+  isCorrect: boolean | null;
+}
+
+const initialState: PasswordState = {
+  value: '',
+  isCorrect: null,
+}
+
+const passwordSlice = createSlice({
+  name: 'password',
+  initialState,
+  reducers: {
+    writeDigit: (state, action: PayloadAction<number>) => {
+      if (state.value.length < 4) {
+        state.value += action.payload;
+      }
+    },
+    deleteDigit: (state) => {
+      state.value = state.value.slice(0, -1)
+    },
+    truePassword: (state) => {
+      state.isCorrect = state.value === '1337';
+    }
+  }
+});
+
+export const {
+  writeDigit,
+  deleteDigit,
+  truePassword
+} = passwordSlice.actions;
+
+export default passwordSlice.reducer;
